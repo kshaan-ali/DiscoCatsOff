43caeab3243601e1858d2d59c9f5e4ab0fc44a9b
Haroon Saifi <haroonsaifi2001@gmail.com>
2025-02-05 18:15:26 +0530

    generated structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import VotingPanel from "./components/VotingPanel";
+import Wave from "./svg/Wave";
 
 function App() {
   return (
     <>
       <VotingPanel />
+      <div className="fixed bottom-0 left-0 w-full overflow-hidden">
+        <Wave />
+      </div>
     </>
   );
 }
--- src/components/VotingPanel.tsx
+++ src/components/VotingPanel.tsx
@@ -1,6 +1,11 @@
 function VotingPanel() {
   return (
-    <div>VotingPanel</div>
+  <section className="max-w-[1000px] mx-auto px-[5vw] py-4">
+      <div className="flex justify-between">
+        <button className=" text-shadow bg-[#e49768] text-white p-4 py-1   shadow-[2px_4px_0px_theme(colors.amber.800)] rounded-lg">Disco Cats</button>
+        <button className=" bg-[#e49768] text-amber-900 rounded-lg font-[Bubblegum_Sans] p-4 py-1  ">Connect Wallet</button>
+      </div>
+    </section>
   )
 }
 
